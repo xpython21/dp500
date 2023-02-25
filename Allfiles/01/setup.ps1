8a7e9a807ac510990675b336c03d117f9055dec4
41L3X M@Raya <82734863+xpython21@users.noreply.github.com>
2023-02-25 15:28:05 +0100

    Update setup.ps1

--- Allfiles/01/setup.ps1
+++ Allfiles/01/setup.ps1
@@ -79,13 +79,16 @@ foreach ($provider in $provider_list){
 # Generate unique random suffix
 [string]$suffix =  -join ((48..57) + (97..122) | Get-Random -Count 7 | % {[char]$_})
 Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
-$resourceGroupName = "dp500-$suffix"
+# $resourceGroupName = "dp500-$suffix"
+$resourceGroupName = Read-Host "Enter the name of a resource group"
 
 # Choose a random region
 Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
-$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
+#$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
+$preferred_list = "northeurope","uksouth","westeurope","westus","westus2"
+
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
     $_.Providers -contains "Microsoft.Sql" -and
