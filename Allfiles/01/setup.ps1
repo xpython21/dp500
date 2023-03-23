819c01fca902d8d371d75f204befc9866f47a8d5
41L3X M@Raya <82734863+xpython21@users.noreply.github.com>
2023-03-23 17:50:53 +0100

    Update setup.ps1

--- Allfiles/01/setup.ps1
+++ Allfiles/01/setup.ps1
@@ -87,7 +87,7 @@ Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
 #$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
-$preferred_list = "northeurope","uksouth","westeurope","westus","westus2"
+$preferred_list = "northeurope","uksouth","westeurope"
 
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
